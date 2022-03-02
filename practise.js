e64396338aaecd42fadba0dc51f4f266a5a03a0e
abdullhsharker <abdullahsharker99@gmail.com>
2022-03-02 23:18:13 +0600

    first commit

---
+++ practise.js
@@ -0,0 +1,15 @@
+// const er variable er man ke kokhono change kora jai na EX:
+
+const name = "abdullah";
+
+// let er man change hoi ,kinto variable er nam kokhono same dewa jabe na:
+
+let age = 30;
+let address = 'Brahmanbaria';
+age = 25;
+
+// arrow function
+
+const total = (num1) => num1 / 5;
+// const totalMOney = total(75);
+console.log(total(75));
